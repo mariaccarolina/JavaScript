a05cfe79e381dad91a914524c57a2801e746df10
Maria Carolina <demycar24@gmail.com>
2023-08-20 16:02:05 -0300

    exercicios loop

--- exercicios loops/script.js
+++ exercicios loops/script.js
@@ -6,6 +6,26 @@ for( let i =0; i <= 5; i++){
   console.log(i) // Retorna de 0 a 5 no console
 }
 
+for (let i = 0; i <= 10; i++) {
+  console.log(i); // retorna de 0 a 10
+}
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i); // retorna de 10 a 0
+}
+
+for (let i = 2; i <= 12; i += 2) {
+  console.log(i); // retorna numeros pares até 12
+}
+
+for (let i = 1; i <= 13; i += 2) {
+  console.log(i); // retorna numeros ímpares de 1 ate 13
+}
+
+for (let i = 10; i <= 100; i += 10) {
+  console.log(i); // retorna numeros de 10 em 10
+}
+
 for(let i = 0; i < 10; i++){
   console.log(i) // Retorna de 0 a 9 no console
   if(i === 7){ // Use o break para sair de um laçoantes da expressao da condição ser false
