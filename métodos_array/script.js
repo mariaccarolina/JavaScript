7bdf93ce9018ddd74f4bbe774639f4c73ab9c861
Maria Carolina <demycar24@gmail.com>
2023-08-24 12:10:04 -0300

    metodos

--- métodos_array/script.js
+++ métodos_array/script.js
@@ -1,19 +1,86 @@
 // Em javascript existem alguns métodos que facilitam o trabalho com arrays;
 
+let lista = [1, 2, 3, 4, 5, 6];
+console.log(lista);
+console.log(lista.length); 
+// 3 Retorna o número de itens no array
+
+lista.push(4); 
+// Adicionou 4 ao final do array
+
+lista.unshift(0); 
+// Adicionou o 0 ao inicio do array
+
+lista.pop();
+ // Remover o ultimo item do array
+
+lista.shift(); 
+// Remove o primeiro item do array
+
+// lista.slice(2, 4); //.slice(inicio, fim)
+let teste = lista.slice(0, 4);
+console.log(teste); 
+// Retorna [1,2,3,4]
+
+console.log(lista.join("-"));
+ // Retorna 1-2-3-4-5-6
+
+// Exibi todo meu array com o que colocar dentro do join
+console.log(lista.join("_"));
+ // Retorna 1_2_3_4_5_6
+
+let centenas = [
+    31,
+    7,
+    122,
+    642,
+    1002,
+    300000,
+    656,
+    789,
+    999,
+    454,
+    565,
+    1,
+    2,
+    3
+  ];
+  // console.log(centenas.sort());
+  console.log( 
+    centenas.sort(function (a, b) {
+      return a - b;
+    })
+  );
+  console.log(typeof centenas);
+// // ordena o array em ordem crescente ou alfabetica
+
 let compras = ["macarrão", "arroz", "filé de frango", "biscoitos", "queijo"]
 console.log(compras)
 
-// compras.push("carne patinho") // Adiciona o item no final do array
-// compras.unshift("miojo") // Adiciona o item no inicio do array
-// compras.pop() // Remove o ultimo item do array
-// compras.shift() // Remove o primeiro item do array
-// compras.reverse() // Inverte a ordem do array
+compras.push("carne patinho")
+ // Adiciona o item no final do array
+
+compras.unshift("miojo")
+ // Adiciona o item no inicio do array
+
+compras.pop()
+ // Remove o ultimo item do array
+
+compras.shift()
+ // Remove o primeiro item do array
+
+compras.reverse() 
+// Inverte a ordem do array
 
 //slice(inicio, fim)
-// let resultado = compras.slice(2) //fatiaria a partir do 2
-// let resultado = compras.slice(1,4) // Retorna do indice um até o 3 
+let resultado = compras.slice(2) 
+//fatiaria a partir do 2
+
+// let resultado = compras.slice(1,4) 
+// Retorna do indice um até o 3 
 // // o indice 4 ele para e não mostra
-// console.log(resultado) // Não desmonta o array original
+// console.log(resultado) 
+// Não desmonta o array original
 
 
 // O splice()método adiciona/remove/substitui elementos do array.
@@ -27,14 +94,6 @@ console.log(compras)
 // const removido = compras.pop()// Para vizualizar qual item eu removi
 // console.log(removido)
 
-// let numeros = [3, 5, 7, 6, 1, 2, 3, 8, 9, 4];
-// console.log(numeros.sort()) // ordena o array em ordem crescente ou alfabetica
-
-
-// const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// console.log(estudantes.sort()) //Ordenou em ordem alfabética;
-
-//
 
 //como saber qual posição(index) de um item no array?
 //usando o indexOf()
